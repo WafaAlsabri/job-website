c73545014efc216522e5157f0c76a960347a4aa4
WafaAlsabri <wafaalsabri23@gmail.com>
2022-02-04 14:52:59 +0300

    fianl

---
+++ js/filter.js
@@ -0,0 +1,51 @@
+//////////////// filtering
+$('.filter').change(function(){
+
+    filter_function();
+    
+    //calling filter function each select box value change
+    
+  });
+  
+  $('.job_card .card .card-body ').show(); //intially all rows will be shown
+  
+  function filter_function(){
+    $('.job_card .card .card-body ').hide(); //hide all rows
+    
+    var companyFlag = 0;
+    var companyValue = $('#filter-company').val();
+    var cityFlag = 0;
+    var cityValue = $('#filter-city').val();
+  
+    //setting intial values and flags needed
+    
+   //traversing each row one by one
+    $('.card .card-body').each(function() {  
+    
+      if(companyValue == 0){   //if no value then display row
+      companyFlag = 1;
+      }
+      else if(companyValue == $(this).find('h5.company').data('company')){ 
+        companyFlag = 1;       //if value is same display row
+      }
+      else{
+        companyFlag = 0;
+      }
+      
+      
+       if(cityValue == 0){
+      cityFlag = 1;
+      }
+      else if(cityValue == $(this).find('span.city').data('city')){
+        cityFlag = 1;
+      }
+      else{
+        cityFlag = 0;
+      }
+  
+     if(companyFlag && cityFlag){
+       $(this).show();  //displaying row which satisfies all conditions
+     }
+  
+  });   
+  }
